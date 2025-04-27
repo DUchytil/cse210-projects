25e9d624b633f72b76431349bb60bbe70d6fc87d
David Uchytil <daviduchytil15@gmail.com>
2025-04-26 22:03:18 -0600

    started prep 2 assignment

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -5,5 +5,6 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep2 World!");
+        Console.WriteLine("I'll get to this assignment a little later! Things got busy.");
     }
 }
