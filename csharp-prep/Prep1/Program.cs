07b1e42d7628019f7333249f2a31b11ce46b7bf1
David Uchytil <daviduchytil15@gmail.com>
2025-04-26 21:55:46 -0600

    finished prep 1

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,10 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        Console.Write("Please enter your first name: ");
+        String firstName = Console.ReadLine();
+        Console.Write("Please enter your last name: ");
+        String lastName = Console.ReadLine();
+        Console.WriteLine($"Your name is: {lastName}, {firstName} {lastName}");
     }
 }
