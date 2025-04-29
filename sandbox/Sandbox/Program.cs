a3d637d06f4bee064d8b6bdaad057f61dbeb14c4
David Uchytil <daviduchytil15@gmail.com>
2025-04-29 10:51:35 -0600

    Add something random

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -5,10 +5,6 @@ class Program
     static void Main(string[] args) // Main function required
     {
 
-        Console.Write("Please enter your first name: ");
-        String firstName = Console.ReadLine();
-        Console.Write("Please enter your last name: ");
-        String lastName = Console.ReadLine();
-        Console.WriteLine($"Your name is: {lastName}, {firstName} {lastName}");
+        Console.WriteLine("This is the thing");
     }
 }
