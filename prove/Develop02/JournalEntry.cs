a265d6affb50c31aae7fe1377351d7c218990908
David Uchytil <daviduchytil15@gmail.com>
2025-05-20 10:48:47 -0600

    fixed problem

---
+++ prove/Develop02/JournalEntry.cs
@@ -0,0 +1,26 @@
+using System;
+
+class JournalEntry
+{
+    
+    
+    public string GetEntryInput()
+    {
+        return "";
+    }
+
+    public void ShowEntry() // include parameter of type 'JournalEntry'
+    {
+        
+    }
+
+    private string GeneratePrompt()
+    {
+        return "";
+    }
+
+    private string GetDateAndTimeAsString()
+    {
+        return "";
+    }
+}
