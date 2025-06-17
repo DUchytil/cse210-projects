177a67155ba4929f224116d41bd63152eeb6af41
David Uchytil <davidu17@byui.edu>
2025-06-16 22:40:13 -0600

    finished prep assignment

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -1,9 +1,24 @@
-using System;
+
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        List<Shape> shapes = new List<Shape>();
+
+        Shape shape1 = new Square("Red", 22);
+        Shape shape2 = new Rectangle("Blue", 22, 33);
+        Shape shape3 = new Circle("Purple", 2);
+
+        shapes.Add(shape1);
+        shapes.Add(shape2);
+        shapes.Add(shape3);
+
+        foreach (Shape shape in shapes)
+        {
+            Console.WriteLine(shape.GetColor()); 
+            Console.WriteLine(shape.GetArea()); 
+        }
+
     }
 }
