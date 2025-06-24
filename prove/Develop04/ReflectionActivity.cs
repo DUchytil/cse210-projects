0dad50bac62a65a8c0344351c1972643b05546a2
David Uchytil <daviduchytil15@gmail.com>
2025-06-24 11:16:24 -0600

    Added cool stuff

--- prove/Develop04/ReflectionActivity.cs
+++ prove/Develop04/ReflectionActivity.cs
@@ -1,5 +1,8 @@
 class ReflectionActivity : BaseActivity
-{    
+{
+    private List<string> _availablePrompts = new List<string>();
+    private List<string> _availableReflectionQuestions = new List<string>();
+
     public ReflectionActivity(string name, string description) : base(name, description)
     {
 
@@ -8,13 +11,16 @@ class ReflectionActivity : BaseActivity
     public override void RunActivity()
     {
         Console.Clear();
+        Console.WriteLine("Get ready... ");
+        base.DisplaySpinner(5);
+        Console.WriteLine("Consider the following prompt: ");
     }
 
     private void SetPromptsAndQuestions()
     {
 
     }
-    private void ResetPromptsUsage()
+    private void ResetPromptsUsage() // Likely not going to be used
     {
         
     }
